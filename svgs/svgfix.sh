5842aa05495223ee8c55590bc7a815ebb2781de8
Harry Fox <1254033+hfoxy@users.noreply.github.com>
2023-10-30 15:45:35 +0000

    update svgs + add svgfix

---
+++ svgs/svgfix.sh
@@ -0,0 +1,43 @@
+set -e
+
+KEYWORD="<style type=\"text/css\">\n\t.track{fill:none;stroke:#231F20;stroke-width:20;stroke-miterlimit:10;}\n</style>"
+ESCAPED_KEYWORD=$(printf '%s\n' "$KEYWORD" | sed -e 's/[]\/$*.^[]/\\&/g')
+
+REPLACE='<style>svg {height: 100%;width: 100%;}.track-surface {fill: none;stroke: white;stroke-width: 30;}</style>'
+ESCAPED_REPLACE=$(printf '%s\n' "$REPLACE" | sed -e 's/[\/&]/\\&/g')
+
+echo 'Replacing .track'
+sed -i '.bak' "s/$ESCAPED_KEYWORD/$ESCAPED_REPLACE/g" *.svg
+
+
+
+KEYWORD='<style type="text/css">.track{fill:none;stroke:#231F20;stroke-width:20;stroke-miterlimit:10;}</style>'
+ESCAPED_KEYWORD=$(printf '%s\n' "$KEYWORD" | sed -e 's/[]\/$*.^[]/\\&/g')
+
+REPLACE='<style>svg {height: 100%;width: 100%;}.track-surface {fill: none;stroke: white;stroke-width: 30;}</style>'
+ESCAPED_REPLACE=$(printf '%s\n' "$REPLACE" | sed -e 's/[\/&]/\\&/g')
+
+echo 'Replacing .track single line'
+sed -i '.bak' "s/$ESCAPED_KEYWORD/$ESCAPED_REPLACE/g" *.svg
+
+
+
+KEYWORD='class="track"'
+ESCAPED_KEYWORD=$(printf '%s\n' "$KEYWORD" | sed -e 's/[]\/$*.^[]/\\&/g')
+
+REPLACE='class="track-surface"'
+ESCAPED_REPLACE=$(printf '%s\n' "$REPLACE" | sed -e 's/[\/&]/\\&/g')
+
+echo 'Replacing class="track"'
+sed -i '.bak' "s/$ESCAPED_KEYWORD/$ESCAPED_REPLACE/g" *.svg
+
+
+
+KEYWORD='st0'
+ESCAPED_KEYWORD=$(printf '%s\n' "$KEYWORD" | sed -e 's/[]\/$*.^[]/\\&/g')
+
+REPLACE='track-surface'
+ESCAPED_REPLACE=$(printf '%s\n' "$REPLACE" | sed -e 's/[\/&]/\\&/g')
+
+echo 'Replacing st0'
+sed -i '.bak' "s/$ESCAPED_KEYWORD/$ESCAPED_REPLACE/g" *.svg
